b384982ccf68c963493f311de8312181b03ae59c
Luc Wicki <Luc_Wicki@sluz.ch>
2022-11-05 14:06:47 +0100

    FooBarQix

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -5,8 +5,43 @@ namespace FooBarQix.Tests
     public class FooBarQixDeterminerTest
     {
         [Fact]
-        public void Test1()
+        public void Determine_WhenNumberNotDivisible()
         {
+            var number = 1; 
+            var testee = new FooBarQixDeterminer();
+
+            var result = testee.Determine(number);
+            Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void Determine_FooWhenNumberDividedby3()
+        {
+            var foo = 3;
+            var testee = new FooBarQixDeterminer();
+
+            var result = testee.Determine(foo);
+            Assert.Equal("Foo", result);
+        }
+
+        [Fact]
+        public void Determine_FooWhenNumberDividedby5()
+        {
+            var bar = 5;
+            var testee = new FooBarQixDeterminer();
+
+            var result = testee.Determine(bar);
+            Assert.Equal("Bar", result);
+        }
+
+        [Fact]
+        public void Determine_FooWhenNumberDividedby7()
+        {
+            var qix = 7;
+            var testee = new FooBarQixDeterminer();
+
+            var result = testee.Determine(qix);
+            Assert.Equal("Qix", result);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -1,7 +1,35 @@
-﻿namespace FooBarQix
+﻿using System;
+
+namespace FooBarQix
 {
     public class FooBarQixDeterminer
     {
 
+        
+
+        public string Determine(int number)
+        {
+            string FBQ = "1";
+            int foo;
+            int bar;
+            int qix;
+            foo = number % 3;
+            bar = number % 5;
+            qix = number % 7;
+            if (foo == 0)
+            {
+                FBQ = "Foo";
+            }
+            else if(bar == 0)
+            {
+                FBQ= "Bar";
+            }
+            else if(qix == 0)
+            {
+                FBQ = "Qix";
+            }
+            
+            return FBQ;
+        }
     }
 }
